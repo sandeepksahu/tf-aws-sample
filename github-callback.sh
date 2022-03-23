58ef361671db80f072065bf92e0487dc39c5c365
Sandeep Sahu <sansahua@amazon.com>
2022-03-22 22:05:33 -0400

    removed github callback script

--- github-callback.sh
+++
@@ -1,9 +0,0 @@
-BODY_PAYLOAD="\`\`\`$@"
-jq -n --arg plan "$BODY_PAYLOAD" '{body: $plan}' > plan.txt
-PR_NO=${CODEBUILD_WEBHOOK_TRIGGER:3}
-curl -X POST \
-  https://api.github.com/repos/sandeepksahu/$REPO/issues/$PR_NO/comments \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: Bearer $TOKEN" \
-  -H "Content-Type: application/json" \
-  -d @plan.txt
