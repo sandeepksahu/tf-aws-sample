4b32579bb5e8372b2b4ca116ab13310d04a38c16
Sandeep Sahu <sansahua@amazon.com>
2022-03-22 21:58:07 -0400

    updated github url

--- github-callback.sh
+++ github-callback.sh
@@ -1,11 +1,8 @@
 BODY_PAYLOAD="\`\`\`$@"
 jq -n --arg plan "$BODY_PAYLOAD" '{body: $plan}' > plan.txt
 PR_NO=${CODEBUILD_WEBHOOK_TRIGGER:3}
-echo $REPO
-echo $PR_NO
-echo $TOKEN
 curl -X POST \
-  https://api.github.com/repos/$REPO/issues/$PR_NO/comments \
+  https://api.github.com/repos/sandeepksahu/$REPO/issues/$PR_NO/comments \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "Content-Type: application/json" \
